84f1648b6fb1db4de7386b6a16c78334159bcfd6
fegonzalez <fer.gonzalez.rodriguez@gmail.com>
2015-05-22 20:31:27 +0200

    Tutorial. section 4 done

---
+++ ferlib/fibonacci.py
@@ -0,0 +1,25 @@
+#!/Users/jc/.virtualenvs/env1/bin/python
+# -*- coding: utf-8 -*-
+
+def fib(n): #Returns the list with the Fibonacci series up to n.
+    """ Returns the list with the Fibonacci series up to n. """
+    retval=[]
+    a, b = 0, 1
+    while a<n:
+        retval.append(a)
+        a, b = b, a+b # Parallel assignment
+    # return retval
+
+
+    ### just to test script. Remove otherwise.
+
+# if __name__ == '__main__':        
+#     assert fib(0)==[]
+#     print("fib(0) = ", fib(0))
+#     expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+#                 987, 1597];
+#     obtained=fib(2000);
+#     assert obtained==expected
+#     print("fib(2000) = ", obtained)
+
+
