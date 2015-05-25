151ba1fcb460e4c80f33f703d5bb4126fe9ab2f6
fegonzalez <fer.gonzalez.rodriguez@gmail.com>
2015-05-25 20:43:46 +0200

    python tutorial.modules (half)

--- ferlib/fibonacci.py
+++ ferlib/fibonacci.py
@@ -8,18 +8,18 @@ def fib(n): #Returns the list with the Fibonacci series up to n.
     while a<n:
         retval.append(a)
         a, b = b, a+b # Parallel assignment
-    # return retval
+    return retval
 
-
-    ### just to test script. Remove otherwise.
-
-# if __name__ == '__main__':        
-#     assert fib(0)==[]
-#     print("fib(0) = ", fib(0))
-#     expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
-#                 987, 1597];
-#     obtained=fib(2000);
-#     assert obtained==expected
-#     print("fib(2000) = ", obtained)
-
+ 
+    ### to test script: this code only runs if the module is executed as script
+if __name__ == '__main__':
+    print("\n... fibonacci.py: script test\n")
+    assert fib(0)==[]
+    print("fib(0) = ", fib(0))
+    expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610,
+                987, 1597];
+    obtained=fib(2000);
+    print("fib(2000) = ", obtained)
+    assert obtained==expected
+    print("\n... test OK\n")
 
